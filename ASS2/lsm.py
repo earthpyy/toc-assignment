5aa3a5c60cd72c0d195584b81cc82ed9e74e8520
Preeti Yuankrathok <preetisatit@gmail.com>
2017-09-06 03:42:12 +0700

    change LS.csv location

--- ASS2/lsm.py
+++ ASS2/lsm.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # load csv into array x, y
-x, y = np.loadtxt('/Users/earth/Documents/CEProjects/ToC/toc-assignment/ASS2/LS.csv', delimiter=',', skiprows=1, unpack=True)
+x, y = np.loadtxt('/Users/EARTHPYY/Documents/CEProjects/ToC/toc-assignment/ASS2/LS.csv', delimiter=',', skiprows=1, unpack=True)
 
 # calculate z and p
 z = np.polyfit(x, y, 1)
 p = np.poly1d(z)
 
 # plotting graph
-xp = np.linspace(min(x), max(x), 100, color='g')
+xp = np.linspace(min(x), max(x), 100)
 plt.plot(x, y, 'r.', xp, p(xp))
 plt.show()
