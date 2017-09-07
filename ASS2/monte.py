656ffb4d9fabf0e3339b1d8f5da5c4ca497d6dc4
Preeti Yuankrathok <preetisatit@gmail.com>
2017-09-07 22:32:15 +0700

    update graph & fix code

--- ASS2/monte.py
+++ ASS2/monte.py
@@ -5,12 +5,19 @@ import matplotlib.pyplot as plt
 # number of darts
 inside = 0
 # total darts to throw
-total = 10000000
+total = 10000
 # enable plot window
-plotEnabled = False
+plotCircleEnabled = True
+plotDevelopEnabled = True
+# plot develop every ... times
+plotTime = 350
+# array of value
+x_pi = range(0, total, plotTime)
+y_pi, x_in, x_out, y_in, y_out = [], [], [], [], []
 
 # initialize plot window
-if plotEnabled:
+if plotCircleEnabled:
+    plt.figure(0)
     plt.axes()
     # create circle with radius=1 and color=blue
     circ = plt.Circle((0, 0), radius=1, color='b', fill=False)
@@ -24,19 +31,36 @@ for i in range(0, total):
     if m.sqrt(x ** 2 + y ** 2) < 1.0:
         inside += 1
         # plot into graph
-        if plotEnabled:
-            plt.plot(x, y, 'g.')
-    elif plotEnabled:
+        if plotCircleEnabled:
+            x_in.append(x)
+            y_in.append(y)
+    elif plotCircleEnabled:
         #plot into graph
-        plt.plot(x, y, 'r.')
+        x_out.append(x)
+        y_out.append(y)
 
-# calculate the last answer
-pi = (float(inside) / total) * 4
+    # calculate pi
+    pi = (float(inside) / (i + 1)) * 4
+
+    # check if it's time to plot
+    if plotDevelopEnabled and i % plotTime == 0:
+        y_pi.append(pi)
+        print(pi)
 
 # print answer in console
+pi = (float(inside) / total) * 4
 print(pi)
 
 # if plot is enable then show it
-if plotEnabled:
+if plotCircleEnabled:
+    plt.figure(0)
+    plt.plot(x_in, y_in, 'g.')
+    plt.plot(x_out, y_out, 'r.')
     plt.axis('scaled')
+    plt.show()
+
+if plotDevelopEnabled:
+    plt.figure(1)
+    plt.plot(x_pi, y_pi)
+    plt.plot(x_pi, y_pi, 'r.')
     plt.show()
--- ASS2/newton.py
+++ ASS2/newton.py
@@ -1,13 +1,18 @@
+import matplotlib.pyplot as plt
+from decimal import *
+
 # number to square root
 number = 13
 # start number
-start = 1
+start = Decimal(3)
 # epsilon
-epsilon = 0.000001
+epsilon = Decimal(0.00000000001)
+# array of value
+y = []
 
 # derivative function
 def derivative(x, h):
-      return (f(x+h) - f(x-h)) / (2.0 * h)
+      return (f(x+h) - f(x-h)) / (Decimal(2) * h)
 
 # function to solve
 def f(x):
@@ -15,15 +20,24 @@ def f(x):
 
 # newton-raphson's method
 def solve(x0, h):
-    lastX = x0
-    nextX = lastX + 10 * h
+    lastX = Decimal(x0)
+    nextX = lastX + Decimal(10) * h
     while (abs(lastX - nextX) > h):
         newY = f(nextX)
         lastX = nextX
         nextX = lastX - newY / derivative(lastX, h)
+        
+        # show value
+        print nextX
+        # add value to array to plotting
+        y.append(nextX)
     return nextX
 
 # call solve function
 answer = solve(start, epsilon)
-# print answer
-print answer
+
+# show graph
+x = list(range(len(y)))
+plt.plot(x, y)
+plt.plot(x, y, 'r.')
+plt.show()
