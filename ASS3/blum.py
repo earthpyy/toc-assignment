17033e887ce692f836ec63fb99de9fdd603cdde2
Preeti Yuankrathok <preetisatit@gmail.com>
2017-10-03 15:55:42 +0700

    update ASS3

--- ASS3/blum.py
+++ ASS3/blum.py
@@ -1,34 +1,40 @@
 import matplotlib.pyplot as plt
 
 # seed of random function
-seed = 69
-# number of digits
-n = len(str(seed))
+seed = 4
+# function parameters
+p = 11
+q = 47
 # max integer
-max_i = 100
+max_i = p * q
 # random times
 r_times = 10**11
 # real random times
-rr_times = 20
+rr_times = 44
 
 # print("0000\r", end="")
-
+s = seed
 ans = [0 for i in range(max_i)]
 for i in range(rr_times):
-    x = (seed**2 % (10 ** (n + n // 2)) // (10 ** (n // 2)))
-    ans[x] += 1
-    seed = x
+    x = (s**2) % max_i
+    # ans[x] += 1
+    ans[x] += (r_times // rr_times)
+    s = x
     # print(format(i + 1, '04d'), "\r", end="")
     # print(x)
 
-# zero forever
-ans[0] += r_times - rr_times
+# repeat forever
+s = seed
+for i in range(r_times % rr_times):
+    x = (s**2) % max_i
+    ans[x] += 1
+    s = x
 
-f = open('middle.txt', 'w')
+f = open('blum.txt', 'w')
 print(ans, file=f)
 f.close()
 
 plt.bar(range(max_i), ans, 0.9, edgecolor='b')
-plt.title("Middle-Square PRNG")
+plt.title("Blum Blum Shub PRNG")
 
 plt.show()
